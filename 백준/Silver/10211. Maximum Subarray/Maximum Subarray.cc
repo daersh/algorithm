7f8df23ffb5f02c67822b4c0327a850546bee660
hyunwoo <daersh@naver.com>
2024-01-10 17:03:29 +0900

    [Silver IV] Title: Maximum Subarray, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/10211. Maximum Subarray/Maximum Subarray.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int T;
+    cin >> T;
+    for(int t=0;t<T;t++)
+    {
+        int n;
+        cin >> n;
+        int arr[n];
+        for(int i=0; i< n; i++){
+            cin >> arr[i];
+        }
+        int max= -1e9;
+        int sum=0;
+        for(int i=0; i< n; i++){
+             
+            sum+=arr[i];
+
+            if(max<sum){
+                max=sum;
+            }
+
+            if(sum<0){
+                sum=0;
+            }
+            
+        }
+        cout << max <<'\n';
+    }
+    
+}
