83e01437b0d79d688fede37475c68ae42548de5f
hyunwoo <daersh@naver.com>
2024-01-11 14:41:14 +0900

    [Gold III] Title: 두 배열의 합, Time: 632 ms, Memory: 56804 KB -BaekjoonHub

---
+++ 백준/Gold/2143. 두 배열의 합/두 배열의 합.cc
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <map>
+
+using namespace std;
+
+
+int main(){
+    long long val;
+    int n,m;
+    long long cnt=0;
+    map<long long,long long> find;
+
+    cin >> val;
+    cin >> n;
+    
+    long long arr1[n+1];
+    arr1[0]=0;
+    for(int i=1;i<=n;i++){
+        cin >> arr1[i];
+        arr1[i]+=arr1[i-1];
+    }
+
+    cin >> m;
+    long long arr2[m+1];
+    arr2[0]=0;
+
+    for(int i=1;i<=m;i++){
+        cin >> arr2[i];
+        arr2[i]+=arr2[i-1];
+    }
+
+    for(int i=1;i<=n;i++){
+        long long start = arr1[i];
+
+        for(int j=0;j<i;j++){
+            long long cmp = start-arr1[j];
+            find[cmp]++;
+        }
+
+    }
+
+
+    for(int i=1;i<=m;i++){
+        long long start2= arr2[i];
+        for(int j=0;j<i;j++){
+            long long cmp = start2-arr2[j];         
+            if(find[val-cmp]!=0){
+                cnt+=find[val-cmp];
+            }
+        }
+    }
+
+    cout << cnt;
+}
