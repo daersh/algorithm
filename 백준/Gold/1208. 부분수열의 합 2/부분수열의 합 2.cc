8146feb2b95b480026de5b13fde907a7e981a050
hyunwoo <daersh@naver.com>
2024-01-14 13:51:53 +0900

    [Gold I] Title: 부분수열의 합 2, Time: 836 ms, Memory: 43736 KB -BaekjoonHub

---
+++ 백준/Gold/1208. 부분수열의 합 2/부분수열의 합 2.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+int n, m;
+int arr[41];
+long long cnt;
+map<int,int> result;
+
+void left(int loc, int sum){
+    //끝에 도달하면 종료
+    if(loc== n/2){
+        result[sum]++;
+        return;
+    }
+    //다음위치
+    left(loc+1,sum);
+    //현재위치+다음위치
+    left(loc+1,sum+arr[loc]);
+}
+void right(int loc, int sum){
+    if(loc==n){
+        //끝에 도달하면 종료
+        cnt+= result[m-sum];
+        return;
+    }
+    right(loc+1,sum);
+    right(loc+1,sum+arr[loc]);
+}
+
+int main(){
+    cin >> n >> m;
+    for(int i=0; i<n; i++){
+        cin >> arr[i];      
+    }
+    left(0,0);
+    right(n/2,0);
+    if(m==0) cnt--;
+    cout << cnt;
+}
