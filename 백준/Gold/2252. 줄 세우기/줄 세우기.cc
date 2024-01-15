57194eb6b9f169a65d9b59e925f76579a52a36ca
hyunwoo <daersh@naver.com>
2024-01-15 10:00:27 +0900

    [Gold III] Title: 줄 세우기, Time: 88 ms, Memory: 3980 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+#include <vector>
+using namespace std;
+typedef pair<int,int > pii;
+
+int main(){
+    int n,m;
+    cin >> n >> m;
+    int arr[n+1];
+    vector<int> vec[n+1];
+    queue<int> q;
+    for(int i=0;i<=n;i++){
+        arr[i]=0;
+    }
+    for(int i=0;i<m;i++){
+        int x,y;
+        cin >> x >> y; 
+        vec[x].push_back(y);
+        arr[y]++;
+    }
+
+    for(int i=1; i<=n; i++){
+        if(arr[i]==0) q.push(i);
+    }
+
+    vector<int> result;
+
+    while (!q.empty())
+    {
+        int loc = q.front();
+        //cout << "loc: " <<loc <<'\n';
+        result.push_back(loc);
+        q.pop();
+        for(int i=0; i< vec[loc].size(); i++){
+            int next=vec[loc][i];
+            arr[next]--;
+            if(arr[next]==0) q.push(next);
+        }
+    }
+
+    for(int i=0; i< n ;i++){
+        cout << result[i]<<' ';
+    }
+}
