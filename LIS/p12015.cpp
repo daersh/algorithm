be42855a1644e96e0a7e90041dea57fb9ff2e026
daersh <daersh@naver.com>
2024-01-17 13:48:59 +0900

    Update p12015.cpp

--- LIS/p12015.cpp
+++ LIS/p12015.cpp
@@ -20,8 +20,7 @@ int eboon(int v){
 }
 
 int main(){
-        ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-
+    ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
     cin >> n;
     
     for(int i=0;i<n;i++){
