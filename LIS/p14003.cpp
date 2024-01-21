b93a58bab6c901f0f9132c9f8f301a0d711a342f
daersh <daersh@naver.com>
2024-01-21 23:23:26 +0900

    s

--- LIS/p14003.cpp
+++ LIS/p14003.cpp
@@ -26,13 +26,11 @@ int main(){
     vector<int> back;
     vector<int> data;
     cin >> n;
-    
-    //for(int i=0;i<n;i++){
     cin >> arr1[0];
     int max=0;
     vec.push_back(arr1[0]);
     back.push_back(0);
-
+    
     for(int i=1; i< n ;i++){
         cin >>arr1[i];
         
@@ -43,29 +41,16 @@ int main(){
         int pos = eboon(arr1[i]);
         vec[pos] = arr1[i];
         back.push_back(pos);
-        
-        if(pos>max)max=pos;
-
-        // for(int i=0;i<vec.size();i++){
-        //     cout <<vec[i]<<' ';
-        // }cout <<'\n';
-        
-        // for(int i=0;i<back.size();i++){
-        //     cout <<back[i]<<' ';
-        // }cout <<'\n';
-        
     }   
     cout << end_p+1<<'\n';
     vector<int>  result;
     for(int i=0; i< n; i++){
         if(back[i]==max){
             max --;
-            //cout << arr1[i]<<' ';
             result.push_back(arr1[i]);
             int past_val= arr1[i];
             for(int j=i-1; j>=0;j--){
                 if(back[j]==max && arr1[j]< past_val){
-                    // cout << arr1[j] << ' ';
                     result.push_back(arr1[j]);
                     max --;
                 }
