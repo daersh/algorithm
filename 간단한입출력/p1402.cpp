d62d80fa315febb1a48440e64beb5d3ef8e6e339
daersh <daersh@naver.com>
2024-01-31 15:26:37 +0900

    a

---
+++ 간단한입출력/p1402.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    for(int i=0; i< n; i++){
+        int x,y;
+        cin >> x >>y;
+
+
+    }
+}
+
+
+/**
+ * case1 : x+1=y 무조건 true
+ * case2 : x+1<y 무조건 false
+ * case3 : x==y 무조건 false 
+ * case4 : 8 7 t 1
+ * case5 : 8 6 t 2
+ * case6 : 8 5 f 3
+ *
+ * case7: 9(1,3,9) 10,6,7,
+ * case8: 10(1,2,5,10) 11,8,7
+ */
