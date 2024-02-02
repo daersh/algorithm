7341c902aa1ff179534b01316318d8fa10686c95
hyunwoo <daersh@naver.com>
2024-02-02 17:35:30 +0900

    [Gold V] Title: 풍선 맞추기, Time: 388 ms, Memory: 5360 KB -BaekjoonHub

---
+++ 백준/Gold/11509. 풍선 맞추기/풍선 맞추기.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <algorithm>
+#include <map>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i=0; i< n ;i++){
+        cin >> arr[i];
+    }
+
+    // priority_queue<int,vector<int>,greater<int> > q;
+    map<int,int> loc;
+    int cnt=1;
+    // q.push(arr[0]);
+    loc[arr[0]-1]=1;
+    for(int i=1; i< n; i++){
+        if(loc[arr[i]]!=0){
+            loc[arr[i]]--;
+        }else {
+            cnt++;
+        }
+        loc[arr[i]-1]++;
+        
+    }
+    cout << cnt;
+}
