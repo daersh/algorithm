b541b9052de0d1b9304a0124254dac8aee47a2a6
hyunwoo <daersh@naver.com>
2024-02-05 08:28:32 +0900

    [Silver V] Title: 뒤집기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1439. 뒤집기/뒤집기.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+string x;
+int cnt[2];
+char past='0';
+
+int main(){
+    
+    cin >> x;
+
+    if(x[0]=='0'){
+        cnt[0]++;
+    }else{
+        cnt[1]++;
+        past='1';
+    }
+
+    for(int i=1; i< x.length();i++){
+        if(x[i]==past){
+            // cout << "   "<< cnt[0]<<' '<<cnt[1]<<'\n';
+            continue;
+        }
+        if(x[i]=='0'){
+            cnt[0]++;
+            past='0';
+        }else{
+            cnt[1]++;
+            past='1';
+        }
+        // cout << cnt[0]<<' '<<cnt[1]<<'\n';
+    }
+
+    cout << min(cnt[0],cnt[1]);
+
+}
