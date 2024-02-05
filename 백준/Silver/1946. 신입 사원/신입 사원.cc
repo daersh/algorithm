b999f1c95a1df76092fd408d455c54e066828349
hyunwoo <daersh@naver.com>
2024-02-05 14:03:20 +0900

    [Silver I] Title: 신입 사원, Time: 1492 ms, Memory: 2288 KB -BaekjoonHub

---
+++ 백준/Silver/1946. 신입 사원/신입 사원.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include<algorithm>
+#include<queue>
+#include<vector>
+#include<cmath>
+
+using namespace std;
+
+int main(){
+    int T;
+    cin >> T;
+
+    for(int t=0; t< T ;t++)
+    {
+        int n;
+        cin >> n;
+        int arr[n+1];
+        for(int i=1; i<=n; i++){
+            int x ,y;
+            cin >>x>>y;
+            arr[x]=y;
+        }
+        int cnt=1;
+        int min=arr[1];
+        for(int i=2; i<=n;i++){
+            int cmp=arr[i];
+            // cout << cmp << " vs " << min <<'\n';
+            if(cmp < min){
+                cnt++;
+                min=cmp;
+            }
+        }
+        cout << cnt<<'\n';
+    }
+}
+
+/*  
+    1 4
+    2 3
+    3 2
+    4 1
+    5 5
+*/
