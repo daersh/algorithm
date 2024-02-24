20712328db22f93bb7932394da976ebe16ff7d5a
daersh <daersh@naver.com>
2024-02-24 21:52:54 +0900

    Create p17396.cpp

---
+++ digkstra/p17396.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <cmath>
+using namespace std;
+
+int arr[100010];
+int visited[100010];
+int d[100010];
+
+int main(){
+    int n, m ;
+    cin >> n >> m;
+
+    for(int i =0 ;i < n ; i++){
+        cin >> arr[i];
+    }
+    
+}
