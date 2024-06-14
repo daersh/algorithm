039679be53566f18f7c5805940f82ecd13c55c67
daersh <daersh@naver.com>
2024-06-14 12:20:28 +0900

    f

---
+++ digkstra/p1446.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+const int INF = 1e9;
+typedef pair<int,int> pii;
+int n,m;
+int visited[10010][10010];
+
+
+int main(){
+
+    cin >> n >> m;
+    for(int i=0; i< n; i++){
+        
+    }
+}
---
+++ digkstra/p5014.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int F,S,G,U,D; // 건물높이 , 시작층, 목표층, 업,다운
+int arr[1000010];
+
+int main(){
+    cin >> F >> S >> G >> U >> D;
+
+
+
+}
+
+//10 1 10 2 1
+// 1-> 3 -> 5 -> 7 -> 9 ->8 ->10
