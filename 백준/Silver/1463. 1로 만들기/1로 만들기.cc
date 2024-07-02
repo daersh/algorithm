a3794f93c0f5bb0bc7f4cc52285477ee1b92bdec
hyunwoo <daersh@naver.com>
2024-07-02 15:53:40 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5804 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -15,24 +15,30 @@ int main(){
      arr[3] = 1;
 
       for(int i =4;i<=x;i++){
-        int min = 1e9;
+        // int min = 1e9;
 
-        if(i%3 ==0 && min>arr[i/3]+1)
-            min = arr[i/3]+1;
+        // if(i%3 ==0 && min>arr[i/3]+1)
+        //     min = arr[i/3]+1;
 
-        if((i-1)%3 == 0 && min>arr[(i-1)/3]+2)
-            min = arr[(i-1)/3]+2;
+        // if((i-1)%3 == 0 && min>arr[(i-1)/3]+2)
+        //     min = arr[(i-1)/3]+2;
 
-        if((i-2)%3 == 0 && min>arr[(i-2)/3]+3)
-            min = arr[(i-2)/3]+3;
+        // if((i-2)%3 == 0 && min>arr[(i-2)/3]+3)
+        //     min = arr[(i-2)/3]+3;
 
-        if((i)%2 ==0 && min >arr[(i/2)]+1)
-            min =arr[(i/2)]+1;
+        // if((i)%2 ==0 && min >arr[(i/2)]+1)
+        //     min =arr[(i/2)]+1;
 
-        if((i-1)%2 == 0 && min > arr[(i-1)/2]+2)
-            min = arr[(i-1)/2]+2;
-
-        arr[i]=min;
+        // if((i-1)%2 == 0 && min > arr[(i-1)/2]+2)
+        //     min = arr[(i-1)/2]+2;
+        
+        // 정석
+        int a = arr[i/3]+ i%3+1;
+        int b = arr[i/2]+ i%2+1;
+        if(a<b)
+            arr[i]=a;
+        else
+            arr[i]=b;
         //cout << i << ": "<<arr[i]<<'\n';
       }
 
