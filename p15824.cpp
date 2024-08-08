2a45271c5dac20e3cef90cbc02ba4bc6bd7f7ade
daersh <daersh@naver.com>
2024-08-08 19:36:47 +0900

    문제 추가

---
+++ p15824.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    
+    int arr[n];
+    for(int i =0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    
+}
