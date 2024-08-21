60371737cdc25cc04d0859402da3d9d00631c0e1
hyunwoo <daersh@naver.com>
2024-08-21 14:03:11 +0900

    [Silver II] Title: 풍선 공장, Time: 840 ms, Memory: 14320 KB -BaekjoonHub

---
+++ 백준/Silver/15810. 풍선 공장/풍선 공장.cc
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+const long long INF = 1e16;
+typedef long long ll;
+
+int main(){
+
+    ll n , m ; // n : 풍선 푸는사람   m: 풍선 갯수
+    cin >> n >> m;
+
+    vector<ll> vec;        // vec 선언
+
+    for(int i = 0; i <n; i++){
+        ll temp;
+        cin >> temp;    // 입력
+        vec.push_back(temp); // 벡터 값 push
+    }
+    
+    ll l = 1;
+    ll r = INF;
+
+    while(l+1<r){
+        ll mid = (l+r)/2;
+
+        // mid: 분 
+        // 몇분일 때 풍선 개수가 몇갠지 찾는거
+        ll cnt = 0;
+
+        for(int i =0; i <n;i++ ){
+            cnt += mid/vec[i];
+        }
+
+
+        if(cnt < m)
+            l = mid;
+        else 
+            r = mid;
+    }
+
+
+    ll lcnt = 0;
+    ll rcnt = 0;
+
+    for( int i = 0 ; i < n ; i++ ){
+        lcnt += l/vec[i];
+        rcnt += r/vec[i];
+    }
+
+    if(lcnt == m)
+        cout << l;
+    else
+        cout << r;
+
+    return 0;
+}
