4b712c1778992e5db0973d4408a48d5d6cd77d41
hyunwoo <daersh@naver.com>
2024-08-22 16:52:22 +0900

    [Gold III] Title: 중량제한, Time: 120 ms, Memory: 7932 KB -BaekjoonHub

---
+++ 백준/Gold/1939. 중량제한/중량제한.cc
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+typedef pair<int, int> pii;
+vector<pii> vec[100010];
+int n , m;
+int visited[100010];
+int s,e;
+int l= 1;
+int r = 1e9;
+
+int bfs(int mid){
+    // mid : 무게
+    
+    queue<int> q;
+    q.push(s);
+
+    while (!q.empty()) 
+    {
+        int loc = q.front();
+        q.pop();
+        
+        if(visited[loc])continue;
+        visited[loc] = true;
+
+        for(int i=0; i<vec[loc].size(); i++){
+            int next_loc = vec[loc][i].first;
+            int next_weight = vec[loc][i].second;
+            
+            if(visited[next_loc] ==0 && next_weight >= mid){
+                q.push(next_loc);
+            }
+        }
+    }
+
+    return visited[e];
+}
+
+int main(){
+
+    cin >> n >> m;
+
+
+    for(int i = 0; i < m; i++){
+        int x , y, z;
+        cin >> x >> y >> z;
+
+        vec[x].push_back(make_pair(y,z));
+        vec[y].push_back(make_pair(x,z));
+    }
+
+    cin >> s >> e;
+
+
+
+    while(l+1<r){
+        int mid = (l+r)/2;
+        fill(&visited[0],&visited[n+1],0);
+        int result = bfs(mid);
+        if(result == false) r = mid;
+        else
+            l=mid;
+
+        // cout << "l = " << l << " r = " << r <<'\n';
+    }
+    
+    fill(&visited[0],&visited[n+1],0);
+    int result = bfs(r);
+    if(result){
+        cout << r;
+    }else{
+        cout << l;
+    }
+}
