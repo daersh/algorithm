b0a2b9550063a16905165b481c5ac19978a8ba52
hyunwoo <daersh@naver.com>
2024-08-24 21:25:48 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+int main (){
+        ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    priority_queue<int, vector<int>, greater<int> > pq;
+    
+    for(int i = 0; i < n; i++){
+        int num;
+        cin >> num;
+        if(num!= 0){
+            // cout <<num<< " push\n";
+            pq.push(num);
+        }else{
+            if(pq.empty()){
+                cout << "0\n";
+            }else{
+                int x = pq.top();
+                cout << x <<'\n';
+                pq.pop();
+            }
+        }
+    }
+
+}
