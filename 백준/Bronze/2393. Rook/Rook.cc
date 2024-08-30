be6de3a32e4f92040cb271daf63484bb2727d9e3
hyunwoo <daersh@naver.com>
2024-08-30 14:03:16 +0900

    [Bronze V] Title: Rook, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    cout << "  ___  ___  ___ \n" ;
+cout << "  | |__| |__| | \n" ;
+  cout << "  |           | \n" ;
+    cout << "   \\_________/ \n" ;
+     cout << "    \\_______/ \n" ;
+      cout << "     |     | \n" ;
+cout <<       "     |     | \n"  ;
+cout <<       "     |     | \n" ;
+cout <<       "     |     | \n" ;
+cout <<       "     |_____| \n" ;
+   cout << "  __/       \\__ \n" ;
+  cout << " /             \\ \n" ;
+ cout << "/_______________\\ \n" ;
+}
