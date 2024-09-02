f5d1130ffbaf2247d9f65970fd2167809adb9dc2
hyunwoo <daersh@naver.com>
2024-09-02 22:17:50 +0900

    [Silver III] Title: 선분 위의 점, Time: 72 ms, Memory: 2920 KB -BaekjoonHub

---
+++ 백준/Silver/11663. 선분 위의 점/선분 위의 점.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <map>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+    int n ,m;
+    cin >> n >> m;
+    vector<int> arr;
+
+    int x;
+
+    for(int i=0; i<n; i++){
+        cin >> x;
+        arr.push_back(x);
+    }
+    sort(arr.begin(), arr.end());
+    int result =0;
+
+    for(int i = 0; i < m; i++){
+        int x, y;
+        cin >> x >> y;
+        int l = lower_bound(arr.begin(), arr.end(),x)-arr.begin();
+        int r = upper_bound(arr.begin(), arr.end(),y)-arr.begin();
+
+        cout << r - l <<'\n';
+    }
+
+
+    // cout << result;
+
+    return 0;
+}
+
+// -6 -5 -4 -4 0 1 2 2 3 7
