7164034c46a8c0ee636fafa3cea306229e3c4ed6
hyunwoo <daersh@naver.com>
2024-10-03 21:09:05 +0900

    [Silver IV] Title: 듣보잡, Time: 40 ms, Memory: 7576 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.cc
+++ 백준/Silver/1764. 듣보잡/듣보잡.cc
@@ -7,6 +7,7 @@ using namespace std;
 
 
 int main(){
+    ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
     set<string> names;
     priority_queue<string, vector<string>,greater<string> >q;
     int n, m, cnt=0;
