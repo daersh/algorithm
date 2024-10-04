16dbf1b16f0be20912ddec997272051a5cdf86ef
hyunwoo <daersh@naver.com>
2024-10-04 22:24:31 +0900

    [Silver I] Title: 단지번호붙이기, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
@@ -1,81 +1,74 @@
-#include <bits/stdc++.h>
-
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <queue>
 using namespace std;
 
-// 가로 세로
-int M, N;
-// data 저장
-int arr[101][101];
-// 방문한 노드인지 확인
-int arr2[101][101];
-int apart[10000000];
-
-//dfs 돌 큐들
-queue<pair<int, int> > q;
-int count1;
-
-void dfs(int x, int y){
-  if(arr2[x][y]==true) return;
-
-  apart[count1]+=1;
-  arr2[x][y]=true;
-  if(x!=0 && arr2[x-1][y]==false && arr[x-1][y]== 1){
-  //  cout << "-> "<< x-1 << "," << y <<' ' ;
-    dfs(x-1,y);
-  }
-  if(x!=N-1 && arr2[x+1][y]==false && arr[x+1][y]== 1){
-  //  cout << "-> "<< x+1 << "," << y <<' ' ;
-    dfs(x+1,y);
-  }
-  if(y!=0 && arr2[x][y-1]==false && arr[x][y-1]== 1){
- //   cout << "-> "<< x << "," << y-1 <<' ' ;
-    dfs(x,y-1);
-  }
-  if(y!=M-1 && arr2[x][y+1]==false && arr[x][y+1]== 1){
-//    cout << "-> "<< x<< "," << y+1 <<' ' ;
-    dfs(x,y+1);
-  }
-
-}
+int dx[4] = {0,0,1,-1};
+int dy[4] = {1,-1,0,0};
 
 int main(){
-  int i, j;
-  int x, y;
 
+    int n;
 
-    memset(arr2,0,sizeof(arr2));
+    cin >> n;
 
-    count1=0;
-    cin >> M;
-    N=M;
-    //배열 입력
-    for(i=0; i<N; i++){
-      for(j=0;j<M;j++){
-        scanf("%1d",&arr[i][j]);
-        if(arr[i][j]==1){
-        //육지일 경우 큐에 담아둠
-          q.push(make_pair(i,j));
+    int arr[n+1][n+1];
+    int visited[n+1][n+1];
+
+    for(int i=0; i<n; i++){
+        string str;
+        cin >> str;
+        for(int j=0; j<n; j++){
+            arr[i][j] = str[j]-'0';
+            visited[i][j] = 0;
         }
-      }
-  }
-
-    int z=q.size();
-    //큐에 담긴 육지들 확인
-  for(i=0;i<z;i++){
-    x= q.front().first;
-    y=q.front().second;
-   // cout <<x<< ","<< y<<" queue 접근\n";
-    q.pop();
-    if(arr2[x][y]==0){
-      //cout <<x<< ","<< y<<" queue 성공: dfs 실행\n";
-      count1++;
-      dfs(x,y);
     }
-  }
-    cout << count1 << '\n';
-    sort(apart, apart+count1+1);
-    for(int i=1;i<count1+1;i++){
-      cout << apart[i]<<'\n';
+
+    priority_queue<int, vector<int>, greater<int> > result;
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+
+            if(arr[i][j] && !visited[i][j] ){
+                
+                int sum = 1;
+                queue<pair<int,int> > q;
+                
+                q.push(make_pair(i,j));
+
+                while(!q.empty()){
+                    int x = q.front().first;
+                    int y = q.front().second;
+                    q.pop();
+
+                    visited[x][y] = true;
+
+                    for(int k=0; k<4; k++){
+                        int nx = x + dx[k];
+                        int ny = y + dy[k];
+
+                        if(nx>=0 && ny>=0 && nx<n && ny<n 
+                            && arr[nx][ny] 
+                            && !visited[nx][ny]){
+                            visited[nx][ny] = true;
+                            sum++;
+                            q.push(make_pair(nx,ny));
+                        }
+                    }
+                }
+                result.push(sum);
+                
+            }
+        }
+    }
+    cout << result.size() <<'\n';
+
+    while(!result.empty()){
+        int a = result.top();
+        result.pop();
+        cout << a <<'\n';
     }
 
 }
