b2990b838d6b8827bdd3b54f53dbf9b91f2beb88
hyunwoo <daersh@naver.com>
2025-12-03 19:07:01 +0900

    [Bronze V] Title: Can you add this?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7891. Can you add this？/Can you add this？.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int x,y;
+    for(int i=0; i<n;i++){
+        cin >> x>>y;
+        cout << x+y<<'\n';
+    }
+}
